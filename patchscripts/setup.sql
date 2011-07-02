e42c7ab4ccdfe9455e1fde0b652bb72ebda0220e
Paul Dragoonis <dragoonis@gmail.com>
2011-07-02 14:34:04 +0100

    Initial SQL file

--- patchscripts/setup.sql
+++ patchscripts/setup.sql
@@ -1,17 +1,147 @@
-CREATE TABLE `users` (
-   `id` int(11) not null auto_increment,
-   `role_id` int(11) not null,
-   `created` int(25) not null,
-   `first_name` varchar(255) not null,
-   `last_name` varchar(255) not null,
-   `email` varchar(255) not null,
-   `password` varchar(255) not null,
-   `username` varchar(255) not null,
-   `active` int(1) not null default '1',
-   `activation_code` varchar(255) not null,
-   `points` int(11),
-   `questions_asked` int(11),
-   `answers_given` int(11),
-   `answers_accepted` int(11),
-   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+# Sequel Pro dump
+# Version 2492
+# http://code.google.com/p/sequel-pro
+#
+# Host: 127.0.0.1 (MySQL 5.1.50)
+# Database: ppi_forum
+# Generation Time: 2011-07-02 14:33:24 +0100
+# ************************************************************
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table answer
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `answer`;
+
+CREATE TABLE `answer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `question_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `answer` text,
+  `votes` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+LOCK TABLES `answer` WRITE;
+/*!40000 ALTER TABLE `answer` DISABLE KEYS */;
+INSERT INTO `answer` (`id`,`question_id`,`user_id`,`answer`,`votes`)
+VALUES
+	(1,1,1,'this is my reply',0);
+
+/*!40000 ALTER TABLE `answer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table badge
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `badge`;
+
+CREATE TABLE `badge` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table question
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `question`;
+
+CREATE TABLE `question` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `notify_user` tinyint(4) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `content` text,
+  `is_answered` tinyint(4) DEFAULT NULL,
+  `is_accepted` tinyint(4) DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `views` int(11) DEFAULT NULL,
+  `votes` int(5) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+LOCK TABLES `question` WRITE;
+/*!40000 ALTER TABLE `question` DISABLE KEYS */;
+INSERT INTO `question` (`id`,`user_id`,`notify_user`,`title`,`content`,`is_answered`,`is_accepted`,`created_date`,`views`,`votes`)
+VALUES
+	(1,1,NULL,'PHP Question','How do you say hello world ?',NULL,NULL,NULL,51,8);
+
+/*!40000 ALTER TABLE `question` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table question_tag
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `question_tag`;
+
+CREATE TABLE `question_tag` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `question_id` int(11) DEFAULT NULL,
+  `tag_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+LOCK TABLES `question_tag` WRITE;
+/*!40000 ALTER TABLE `question_tag` DISABLE KEYS */;
+INSERT INTO `question_tag` (`id`,`question_id`,`tag_id`)
+VALUES
+	(1,8,2),
+	(2,8,1),
+	(3,9,3),
+	(4,9,2),
+	(5,10,3),
+	(6,10,2),
+	(7,11,0),
+	(8,1,2);
+
+/*!40000 ALTER TABLE `question_tag` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table tag
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `tag`;
+
+CREATE TABLE `tag` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+LOCK TABLES `tag` WRITE;
+/*!40000 ALTER TABLE `tag` DISABLE KEYS */;
+INSERT INTO `tag` (`id`,`title`)
+VALUES
+	(1,'css'),
+	(2,'html'),
+	(3,'php');
+
+/*!40000 ALTER TABLE `tag` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
